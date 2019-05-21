e26aa2aa2445a4972278c7ea959f0fb86142f69f
Hemin Johnson <achayan1992@gmail.com>
2019-05-21 19:51:56 +0200

    feat: add route against the test until the test passes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 require('./db/mongoose')
+const productsRouter = require('./routers/products')
 
 const app = express()
 
 app.use(express.json())
+app.use(productsRouter)
 
 module.exports = app
---
+++ routers/products.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const Products = require('../models/products')
+const router = new express.Router()
+
+router.get('/products/:consumption', async (req, res) => {
+  const consumption = req.params.consumption
+  const annualCostProductA = (5*12) + (consumption * .22)
+  const annualCostProductB = (consumption > 4000) ? (800 + (consumption - 4000)*.30) : 800
+
+  const productA = await Products.find({
+    'type': 'A',
+    'annualCosts': annualCostProductA
+  })
+  const productB = await Products.find({
+    'type': 'B',
+    'annualCosts': annualCostProductB
+  })
+
+  if (!(productA.length > 0)) {
+    return res.status(404).send({ error: `Product of type A with an annual cost of ${annualCostProductA} not present in the database` })
+  }
+
+  if (!(productB.length > 0)) {
+    return res.status(404).send({ error: `Product of type B with an annual cost of ${annualCostProductB} not present in the database` })
+  }
+})
+
+module.exports = router
