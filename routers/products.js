637890a0a7f66d9d131a47423dcbc0b3d38c01cd
Hemin Johnson <achayan1992@gmail.com>
2019-05-21 20:16:10 +0200

    feat: add endpoint returns comparison of products based on consumption

--- routers/products.js
+++ routers/products.js
@@ -4,24 +4,38 @@ const router = new express.Router()
 
 router.get('/products/:consumption', async (req, res) => {
   const consumption = req.params.consumption
-  const annualCostProductA = (5*12) + (consumption * .22)
-  const annualCostProductB = (consumption > 4000) ? (800 + (consumption - 4000)*.30) : 800
 
-  const productA = await Products.find({
-    'type': 'A',
-    'annualCosts': annualCostProductA
-  })
-  const productB = await Products.find({
-    'type': 'B',
-    'annualCosts': annualCostProductB
-  })
-
-  if (!(productA.length > 0)) {
-    return res.status(404).send({ error: `Product of type A with an annual cost of ${annualCostProductA} not present in the database` })
+  if(!consumption) {
+    return res.status(404).send({ error: `consumption is required as a parameter which is a number`})
   }
 
-  if (!(productB.length > 0)) {
-    return res.status(404).send({ error: `Product of type B with an annual cost of ${annualCostProductB} not present in the database` })
+  try {
+    const annualCostProductA = (5*12) + (consumption * .22)
+    const annualCostProductB = (consumption > 4000) ? (800 + (consumption - 4000)*.30) : 800
+
+    const productA = await Products.find({
+      'type': 'A',
+      'annualCosts': annualCostProductA
+    })
+    const productB = await Products.find({
+      'type': 'B',
+      'annualCosts': annualCostProductB
+    })
+
+    if (!(productA.length > 0)) {
+      return res.status(404).send({ error: `Product of type A with an annual cost of ${annualCostProductA} not present in the database` })
+    }
+
+    if (!(productB.length > 0)) {
+      return res.status(404).send({ error: `Product of type B with an annual cost of ${annualCostProductB} not present in the database` })
+    }
+
+    res.send({
+      productA,
+      productB
+    })
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
